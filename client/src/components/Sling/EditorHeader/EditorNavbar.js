da46d578937be0c409b3cf645bfc8f484e34f6e6
Jake Perez <jacobanthony.perez@gmail.com>
2017-11-17 17:51:13 -0800

    [feat] - add chat feature

--- client/src/components/Sling/EditorHeader/EditorNavbar.js
+++ client/src/components/Sling/EditorHeader/EditorNavbar.js
@@ -5,9 +5,7 @@ import jwtDecode from 'jwt-decode';
 const EditorNavbar = () => (
   <nav className="editor-navbar">
     <ul>
-      <li id='username'>{jwtDecode(localStorage.token).username}</li>
-      <li>Item 2</li>
-      <li>Item 3</li>
+      <h3 id='username'>{jwtDecode(localStorage.token).username}</h3>
     </ul>
   </nav>
 );
--- rest-server/src/index.js
+++ rest-server/src/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
+import path from 'path';
 
 import log from './lib/log';
 import router from './routes';
